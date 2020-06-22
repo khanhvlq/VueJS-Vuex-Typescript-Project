ea48b063d42b2934767ec47638dca88caf07e2e0
Khanh Vo <khanh.vo@mservice.com.vn>
2020-06-22 16:44:32 +0700

    init

---
+++ src/components/Login.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="login">
+        <form @submit.prevent="login">
+            <h1>Login</h1>
+            <label for="Email">Username</label>
+            <br>
+            <input required v-model="email" id="Email" type="text"/>
+            <br>
+            <br>
+            <label for="Password">Password</label>
+            <br>
+            <input required v-model="password" id="Password" type="password"/>
+            <br>
+            <br>
+            <button type="submit">Login</button>
+        </form>
+    </div>
+
+</template>
+
+<script lang="ts">
+    import { Component, Vue, Prop } from 'vue-property-decorator';
+    import {AUTH_REQUEST} from "@/type"
+
+    @Component
+    export default class Login extends Vue {
+        private email: any = 'eve.holt@reqres.in'
+        private password: any = 'cityslicka'
+        public login() {
+            const users = {
+                email: this.email,
+                password: this.password
+            }
+
+            this.$store.dispatch(AUTH_REQUEST, users).then(() => {
+                this.$router.push('/')
+            })
+        }
+
+        // name: "Login",
+        // methods: {
+        //     login: () => {
+        //         const {username, password} = this
+        //         this.$store.dispatch(AUTH_REQUEST, {username, password}).then(() => {
+        //             this.$router.push("/")
+        //         })
+        //     }
+        // }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,64 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
+import {AUTH_LOGOUT, AUTH_ERROR, AUTH_SUCCESS, AUTH_REQUEST, USER_REQUEST} from "@/type"
 
 Vue.use(Vuex);
 
+const user = {
+    "email": "eve.holt@reqres.in",
+    "password": "cityslicka"
+}
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+    state: {
+        token: localStorage.getItem('user-token') || '',
+        status: ''
+    },
+    mutations: {
+        [AUTH_REQUEST]: (state) => {
+            state.status = 'loading'
+        },
+        [AUTH_SUCCESS]: (state, token) => {
+            state.status = 'success'
+            state.token = token
+        },
+        [AUTH_ERROR]: (state) => {
+            state.status = 'error'
+        }
+    },
+    actions: {
+        [AUTH_REQUEST]: ({commit, dispatch}, user) => {
+            return new Promise((resolve, reject) => {
+                commit(AUTH_REQUEST)
+                console.log(user)
+                axios({url: 'https://reqres.in/api/login', data: user, method: 'POST'})
+                    .then(resp => {
+                        console.log(resp)
+                        const token = resp.data.token
+                        localStorage.setItem('user-token', token)
+                        commit(AUTH_SUCCESS, token)
+                        dispatch(USER_REQUEST)
+                        resolve(resp)
+                    })
+                    .catch(err => {
+                        commit(AUTH_ERROR, err)
+                        localStorage.removeItem('user-token')
+                        reject(err)
+                    })
+            })
+        },
+        [AUTH_LOGOUT]: ({commit, dispatch}) => {
+            return new Promise((resolve, reject) => {
+                commit(AUTH_LOGOUT)
+                localStorage.removeItem('user-token')
+                resolve()
+            })
+        }
+    },
+    modules: {},
+    getters: {
+        isAuth: state => !!state.token,
+        statusAuth: state => !!state.status
+    }
 });
